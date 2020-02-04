9401ee790e412428ebd675eb065779a6610769c4
rajaramk8 <rajaram.class@gmail.com>
2020-02-04 15:18:15 +0530

    users are getting registered in this version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
-      this.intercom.displayMessenger();
+      // this.intercom.displayMessenger();
     });
   }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Intercom } from '@ionic-native/intercom/ngx';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,24 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(    private platform: Platform,
+                  private intercom: Intercom
+    ) {}
+
+showintercommessenger() {
+  this.platform.ready().then(() => {
+    let randomnumber = Math.random() * 9804;
+    let randomnumber1 = randomnumber.toFixed(0);
+    let emailid= 'raj' + randomnumber1 + '@gmail.com';
+
+    console.log('Flow reached here.. 101 ', emailid);
+    this.intercom.registerIdentifiedUser({userId : emailid}).then(
+      () => {
+      console.log('Flow reached here.. 103 ', randomnumber);
+      this.intercom.setLauncherVisibility('VISIBLE');
+            });
+  });
+
+}
 
 }
